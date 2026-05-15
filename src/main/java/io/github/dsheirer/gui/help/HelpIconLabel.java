d9240bdf23dc225925af7e2f31fa387ecdc6d5d0
snepple <25994154+snepple@users.noreply.github.com>
2026-05-15 15:56:14 +0000

    Migrate FilterEditor and leaf panels to JavaFX
    - Converted FilterEditor from JFrame to JavaFX Stage with FXML.
    - Converted FilterEditorPanel from JFXPanel containing Swing JTree to JavaFX BorderPane containing TreeView with FXML.
    - Updated HelpIconLabel to extend JavaFX Label using IconNode.
    - Removed legacy Swing DbPowerMeter in favor of DbPowerMeterJFX.
    - Updated HistoryManagementPanel to properly invoke the new JavaFX FilterEditor Stage.

--- src/main/java/io/github/dsheirer/gui/help/HelpIconLabel.java
+++ src/main/java/io/github/dsheirer/gui/help/HelpIconLabel.java
@@ -1,15 +1,22 @@
 package io.github.dsheirer.gui.help;
 
-import jiconfont.swing.IconFontSwing;
+import jiconfont.javafx.IconNode;
 import jiconfont.icons.font_awesome.FontAwesome;
-import javax.swing.JLabel;
-import javax.swing.ToolTipManager;
-import java.awt.Color;
+import javafx.scene.control.Label;
+import javafx.scene.control.Tooltip;
+import javafx.scene.paint.Color;
 
-public class HelpIconLabel extends JLabel {
+
+
+public class HelpIconLabel extends Label {
     public HelpIconLabel(String htmlHelpText) {
-        super(IconFontSwing.buildIcon(FontAwesome.INFO_CIRCLE, 14, Color.GRAY));
-        setToolTipText(htmlHelpText);
+        IconNode iconNode = new IconNode(FontAwesome.INFO_CIRCLE);
+        iconNode.setIconSize(14);
+        iconNode.setFill(Color.GRAY);
+        setGraphic(iconNode);
 
+        // Remove <html> wrapper if present as JavaFX Tooltip doesn't process html directly, but supports multiline text
+        String tooltipText = htmlHelpText.replace("<html>", "").replace("</html>", "").replace("<b>", "").replace("</b>", "").replace("<br>", "\n");
+        setTooltip(new Tooltip(tooltipText));
     }
 }
