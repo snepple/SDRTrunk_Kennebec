222e21043da57bc1792a902ecd5c2e495a7ca3a9
MadMac85 <sam.nepple@gmail.com>
2026-06-07 15:32:08 -0400

    Fix WindowsHostOptimizer UAC/Powershell exit code

--- src/main/java/io/github/dsheirer/gui/preference/diagnostics/WindowsHostOptimizer.java
+++ src/main/java/io/github/dsheirer/gui/preference/diagnostics/WindowsHostOptimizer.java
@@ -55,13 +55,17 @@ public class WindowsHostOptimizer {
             String sdrTrunkDir = Paths.get(System.getProperty("user.home"), "SDRTrunk").toString();
             
             String script = String.format(
+                "$ErrorActionPreference = 'SilentlyContinue'; " +
+                "try { " +
                 "powercfg /SETACVALUEINDEX SCHEME_CURRENT 2a737441-1930-4402-8d77-b2bea12814ab 48e6b7a6-50f5-4782-a5d4-53bb8f07e226 0; " +
                 "powercfg /SETDCVALUEINDEX SCHEME_CURRENT 2a737441-1930-4402-8d77-b2bea12814ab 48e6b7a6-50f5-4782-a5d4-53bb8f07e226 0; " +
                 "powercfg /SETACTIVE SCHEME_CURRENT; " +
                 "Add-MpPreference -ExclusionPath '%s'; " +
                 "Add-MpPreference -ExclusionPath '%s'; " +
                 "Stop-Service -Name '*DPTF*' -ErrorAction SilentlyContinue; " +
-                "Set-Service -Name '*DPTF*' -StartupType Disabled -ErrorAction SilentlyContinue;",
+                "Set-Service -Name '*DPTF*' -StartupType Disabled -ErrorAction SilentlyContinue; " +
+                "} catch { } " +
+                "exit 0;",
                 currentDir, sdrTrunkDir
             );
             String encoded = Base64.getEncoder().encodeToString(script.getBytes(StandardCharsets.UTF_16LE));
