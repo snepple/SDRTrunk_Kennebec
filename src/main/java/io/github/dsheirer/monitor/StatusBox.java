1a2e49efb2f623d4beaa007cc53f6c6d85b2461a
MadMac85 <sam.nepple@gmail.com>
2026-06-07 16:31:02 -0400

    Fix Resource Status bar: compact labels, narrow bars, prevent truncation

--- src/main/java/io/github/dsheirer/monitor/StatusBox.java
+++ src/main/java/io/github/dsheirer/monitor/StatusBox.java
@@ -40,77 +40,80 @@ public class StatusBox extends HBox
     public StatusBox(ResourceMonitor resourceMonitor)
     {
         mResourceMonitor = resourceMonitor;
-        setPadding(new Insets(1, 0, 1, 0));
-        setSpacing(6);
-        Label cpuLabel = new Label("CPU:");
-        cpuLabel.setPadding(new Insets(0, 0, 0, 10));
-        cpuLabel.setAlignment(Pos.CENTER_RIGHT);
+        setPadding(new Insets(1, 4, 1, 4));
+        setSpacing(3);
+        setAlignment(Pos.CENTER_LEFT);
+
+        // --- CPU ---
+        Label cpuLabel = createLabel("CPU:", "Java process CPU usage. Disabled if the CPU loading is not available from the OS");
         getChildren().add(cpuLabel);
 
-        ProgressBar cpuIndicator = new ProgressBar();
+        ProgressBar cpuIndicator = createBar(60);
         cpuIndicator.progressProperty().bind(mResourceMonitor.cpuPercentageProperty());
         cpuIndicator.disableProperty().bind(mResourceMonitor.cpuAvailableProperty().not());
-        cpuIndicator.setTooltip(new Tooltip("Java process CPU usage. Disabled if the CPU loading is not available from the OS"));
+        cpuIndicator.setTooltip(new Tooltip("Java process CPU usage"));
         getChildren().add(cpuIndicator);
 
-        Label memoryLabel = new Label("Allocated Memory:");
-        memoryLabel.setAlignment(Pos.CENTER_RIGHT);
+        // --- Allocated Memory ---
+        Label memoryLabel = createLabel("Alloc:", "Percentage of total system memory that Java has reserved from the Operating System");
         getChildren().add(memoryLabel);
 
-        ProgressBar memoryBar = new ProgressBar();
+        ProgressBar memoryBar = createBar(60);
         memoryBar.progressProperty().bind(mResourceMonitor.systemMemoryUsedPercentageProperty());
-        memoryBar.setTooltip(new Tooltip("Percentage of total system memory that Java has reserved from the Operating System."));
+        memoryBar.setTooltip(new Tooltip("Allocated Memory - Percentage of total system memory reserved by Java"));
         getChildren().add(memoryBar);
 
-        Label javaMemoryLabel = new Label("Used Memory:");
-        javaMemoryLabel.setAlignment(Pos.CENTER_RIGHT);
+        // --- Used Memory ---
+        Label javaMemoryLabel = createLabel("Used:", "Percentage of allocated memory that Java/sdrtrunk is currently using");
         getChildren().add(javaMemoryLabel);
 
-        ProgressBar javaMemoryBar = new ProgressBar();
+        ProgressBar javaMemoryBar = createBar(60);
         javaMemoryBar.progressProperty().bind(mResourceMonitor.javaMemoryUsedPercentageProperty());
-        javaMemoryBar.setTooltip(new Tooltip("Percentage of allocated memory that Java/sdrtrunk is currently using. This value fluctuates as Java manages memory and garbage collection"));
+        javaMemoryBar.setTooltip(new Tooltip("Used Memory - Percentage of allocated memory in use. Fluctuates with garbage collection"));
         getChildren().add(javaMemoryBar);
 
-        Label eventLogsLabel = new Label("Event Logs:");
-        eventLogsLabel.setAlignment(Pos.CENTER_RIGHT);
+        // --- Event Logs ---
+        Label eventLogsLabel = createLabel("Logs:", "Percentage of drive space used for event logs based on user-specified max threshold");
         getChildren().add(eventLogsLabel);
 
-        ProgressBar eventLogsBar = new ProgressBar();
+        ProgressBar eventLogsBar = createBar(50);
         eventLogsBar.progressProperty().bind(mResourceMonitor.directoryUsePercentEventLogsProperty());
-        eventLogsBar.setTooltip(new Tooltip("Percentage of drive space used for event logs based on user-specified max threshold in user preferences"));
+        eventLogsBar.setTooltip(new Tooltip("Event Logs - Drive space usage vs. max threshold in user preferences"));
         getChildren().add(eventLogsBar);
 
         Label eventLogsSizeLabel = new Label();
         eventLogsSizeLabel.textProperty().bind(mResourceMonitor.fileSizeEventLogsProperty());
-        eventLogsSizeLabel.setAlignment(Pos.CENTER_RIGHT);
+        eventLogsSizeLabel.setMinWidth(javafx.scene.layout.Region.USE_PREF_SIZE);
+        eventLogsSizeLabel.setStyle("-fx-font-size: 10px;");
         getChildren().add(eventLogsSizeLabel);
 
-        Label recordingsLabel = new Label("Recordings:");
-        recordingsLabel.setPadding(new Insets(0, 0, 0, 10));
-        recordingsLabel.setAlignment(Pos.CENTER_RIGHT);
+        // --- Recordings ---
+        Label recordingsLabel = createLabel("Rec:", "Percentage of drive space used for recordings based on user-specified max threshold");
         getChildren().add(recordingsLabel);
 
-        ProgressBar recordingsBar = new ProgressBar();
+        ProgressBar recordingsBar = createBar(50);
         recordingsBar.progressProperty().bind(mResourceMonitor.directoryUsePercentRecordingsProperty());
-        recordingsBar.setTooltip(new Tooltip("Percentage of drive space used for recordings based on user-specified max threshold in user preferences"));
+        recordingsBar.setTooltip(new Tooltip("Recordings - Drive space usage vs. max threshold in user preferences"));
         getChildren().add(recordingsBar);
 
         Label recordingsSizeLabel = new Label();
         recordingsSizeLabel.textProperty().bind(mResourceMonitor.fileSizeRecordingsProperty());
-        recordingsSizeLabel.setAlignment(Pos.CENTER_RIGHT);
+        recordingsSizeLabel.setMinWidth(javafx.scene.layout.Region.USE_PREF_SIZE);
+        recordingsSizeLabel.setStyle("-fx-font-size: 10px;");
         getChildren().add(recordingsSizeLabel);
-        
-        Label droppedBuffersLabel = new Label("Dropped Buffers:");
-        droppedBuffersLabel.setPadding(new Insets(0, 0, 0, 10));
-        droppedBuffersLabel.setAlignment(Pos.CENTER_RIGHT);
+
+        // --- Dropped Buffers ---
+        Label droppedBuffersLabel = createLabel("Drops:", "Total number of native buffers dropped across all tuners due to processor overload");
         getChildren().add(droppedBuffersLabel);
-        
+
         Label droppedBuffersValue = new Label();
         droppedBuffersValue.textProperty().bind(mResourceMonitor.droppedBuffersProperty().asString());
-        droppedBuffersValue.setTooltip(new Tooltip("Total number of native buffers dropped across all tuners due to processor overload"));
-        droppedBuffersValue.setAlignment(Pos.CENTER_RIGHT);
+        droppedBuffersValue.setTooltip(new Tooltip("Dropped Buffers - Native buffers dropped due to processor overload"));
+        droppedBuffersValue.setMinWidth(javafx.scene.layout.Region.USE_PREF_SIZE);
+        droppedBuffersValue.setStyle("-fx-font-size: 10px;");
         getChildren().add(droppedBuffersValue);
 
+        // --- Remote Desktop Indicator ---
         jiconfont.javafx.IconNode remoteIcon = new jiconfont.javafx.IconNode(jiconfont.icons.font_awesome.FontAwesome.DESKTOP);
         remoteIcon.setIconSize(14);
         Label remoteLabel = new Label("", remoteIcon);
@@ -126,6 +129,31 @@ public class StatusBox extends HBox
                 io.github.dsheirer.eventbus.MyEventBus.getGlobalEventBus().unregister(this);
             }
         });
+    }
+
+    /**
+     * Creates a compact, non-truncating label with a tooltip.
+     */
+    private Label createLabel(String text, String tooltipText)
+    {
+        Label label = new Label(text);
+        label.setMinWidth(javafx.scene.layout.Region.USE_PREF_SIZE);
+        label.setTooltip(new Tooltip(tooltipText));
+        label.setStyle("-fx-font-size: 10px; -fx-font-weight: bold;");
+        return label;
+    }
+
+    /**
+     * Creates a narrow progress bar with a fixed preferred width.
+     */
+    private ProgressBar createBar(double width)
+    {
+        ProgressBar bar = new ProgressBar();
+        bar.setPrefWidth(width);
+        bar.setMaxWidth(width);
+        bar.setPrefHeight(14);
+        bar.setMaxHeight(14);
+        return bar;
     }
 
     @com.google.common.eventbus.Subscribe
