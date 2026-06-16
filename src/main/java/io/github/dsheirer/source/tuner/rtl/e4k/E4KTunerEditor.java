397787d48337cb47f0c24596007a55542569944b
snepple <25994154+snepple@users.noreply.github.com>
2026-06-16 05:53:15 +0000

    📏 Auditor: Standardize padding and gaps in ChannelSpectrumPanel

--- src/main/java/io/github/dsheirer/source/tuner/rtl/e4k/E4KTunerEditor.java
+++ src/main/java/io/github/dsheirer/source/tuner/rtl/e4k/E4KTunerEditor.java
@@ -350,10 +350,14 @@ public class E4KTunerEditor extends TunerEditor<RTL2832Tuner, E4KTunerConfigurat
 
                     if(gain == E4KGain.MANUAL)
                     {
-                        getMixerGainCombo().setValue(getEmbeddedTuner().getMixerGain(true));
-                        getMixerGainCombo().setDisable(!(true));
-                        getLNAGainCombo().setValue(getEmbeddedTuner().getLNAGain(true));
-                        getLNAGainCombo().setDisable(!(true));
+                        try {
+                            getMixerGainCombo().setValue(getEmbeddedTuner().getMixerGain(true));
+                            getMixerGainCombo().setDisable(!(true));
+                            getLNAGainCombo().setValue(getEmbeddedTuner().getLNAGain(true));
+                            getLNAGainCombo().setDisable(!(true));
+                        } catch (UsbException ex) {
+                            mLog.error("Error communicating with E4K tuner", ex);
+                        }
                     }
                     else
                     {
